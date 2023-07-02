4696dbda680eeb96b8d9d6b90099c238e1cd3959
DELL <anjugupta7060@gmail.com>
2023-07-02 20:21:39 +0530

    Adding Provider

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
+import 'package:ecom_app/utils/color.dart';
+import 'package:ecom_app/utils/constants.dart';
 import 'package:flutter/material.dart';
-
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,13 +9,54 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  late final TabController _tabController = TabController(length: 3, vsync: this);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home"),
+        body: SizedBox(
+      height: MediaQuery.of(context).size.height,
+      child: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(16, 45, 0, 0),
+            height: MediaQuery.of(context).size.height * 0.4,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/top_img1.png"),
+                    fit: BoxFit.fill)),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Shoes",
+                      style: appStyleWithHt(42, wColor, FontWeight.bold, 1.2),
+                    ),
+                    Text(
+                      "Collection",
+                      style: appStyleWithHt(42, wColor, FontWeight.bold, 1.2),
+                    )
+                  ]),
+            ),
+          ),
+          TabBar(
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorColor: Colors.transparent,
+            controller: _tabController,
+            isScrollable: true,
+            labelStyle: appStyle(24, wColor, FontWeight.bold),
+            tabs: [
+            Tab(text: "Mens Shoes",),
+            Tab(text: "Womens Shoes",),
+            Tab(text: "kids Shoes",),
+          ],),
+          TabBarView(
+            controller: _tabController,
+            children: [],),
+        ],
       ),
-    );
+    ));
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,3 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 TextStyle appStyle(double size, Color color, FontWeight fw) {
   return GoogleFonts.poppins(fontSize: size,color: color,fontWeight: fw);
 }
+
+TextStyle appStyleWithHt(double size, Color color, FontWeight fw,double ht) {
+  return GoogleFonts.poppins(fontSize: size,color: color,fontWeight: fw,height: ht);
+}
