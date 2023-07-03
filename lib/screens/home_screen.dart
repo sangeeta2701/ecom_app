e7194a16b969594fdff590566a1efff28f8dc702
DELL <anjugupta7060@gmail.com>
2023-07-03 20:43:18 +0530

    home screen ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:ecom_app/utils/color.dart';
 import 'package:ecom_app/utils/constants.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +11,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
-  late final TabController _tabController = TabController(length: 3, vsync: this);
+  late final TabController _tabController =
+      TabController(length: 3, vsync: this);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +21,11 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
       child: Stack(
         children: [
           Container(
-            padding: EdgeInsets.fromLTRB(16, 45, 0, 0),
+            padding: EdgeInsets.fromLTRB(16, 35, 0, 0),
             height: MediaQuery.of(context).size.height * 0.4,
             decoration: BoxDecoration(
                 image: DecorationImage(
-                    image: AssetImage("assets/images/top_img1.png"),
+                    image: AssetImage("assets/images/top_image.png"),
                     fit: BoxFit.fill)),
             child: SizedBox(
               width: MediaQuery.of(context).size.width,
@@ -32,29 +34,134 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                   children: [
                     Text(
                       "Shoes",
-                      style: appStyleWithHt(42, wColor, FontWeight.bold, 1.2),
+                      style: appStyleWithHt(42, wColor, FontWeight.bold, 1.0),
                     ),
                     Text(
                       "Collection",
-                      style: appStyleWithHt(42, wColor, FontWeight.bold, 1.2),
-                    )
+                      style: appStyleWithHt(42, wColor, FontWeight.bold, 1.0),
+                    ),
+                    TabBar(
+                      indicatorSize: TabBarIndicatorSize.label,
+                      indicatorColor: Colors.transparent,
+                      controller: _tabController,
+                      isScrollable: true,
+                      labelStyle: appStyle(24, wColor, FontWeight.bold),
+                      unselectedLabelColor: gColor.withOpacity(0.3),
+                      tabs: [
+                        Tab(
+                          text: "Mens Shoes",
+                        ),
+                        Tab(
+                          text: "Womens Shoes",
+                        ),
+                        Tab(
+                          text: "kids Shoes",
+                        ),
+                      ],
+                    ),
                   ]),
             ),
           ),
-          TabBar(
-            indicatorSize: TabBarIndicatorSize.label,
-            indicatorColor: Colors.transparent,
-            controller: _tabController,
-            isScrollable: true,
-            labelStyle: appStyle(24, wColor, FontWeight.bold),
-            tabs: [
-            Tab(text: "Mens Shoes",),
-            Tab(text: "Womens Shoes",),
-            Tab(text: "kids Shoes",),
-          ],),
-          TabBarView(
-            controller: _tabController,
-            children: [],),
+          Padding(
+            padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.265),
+            child: Container(
+              padding: EdgeInsets.only(left: 12),
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.405,
+                        child: ListView.builder(
+                            itemCount: 6,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  color: gColor,
+                                  height: MediaQuery.of(context).size.height,
+                                  width: MediaQuery.of(context).size.width*0.6,
+                                ),
+                              );
+                              
+                            }),
+                      ),
+                      Column(
+                        children: [
+                          Padding(
+                            padding:  EdgeInsets.fromLTRB(12, 20, 12, 20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Latest Shoes",
+                                  style: appStyle(24, bColor, FontWeight.bold),
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      "Show All",
+                                      style:
+                                          appStyle(22, bColor, FontWeight.w600),
+                                    ),
+                                    Icon(
+                                      Icons.arrow_forward_ios,
+                                      size: 20,
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.13,
+                        child: ListView.builder(
+                            itemCount: 6,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: gColor,
+                                    borderRadius: BorderRadius.circular(6)
+                                  ),
+                                  
+                                  height: MediaQuery.of(context).size.height*0.12,
+                                  width: MediaQuery.of(context).size.width*0.28,
+                                  child: CachedNetworkImage(imageUrl: "https://d326fntlu7tb1e.cloudfront.net/uploads/58282ea3-b815-4d26-9f4f-382aa62f67cf-HP5404_a1.webp"),
+                                ),
+                              );
+                              
+                            }),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.405,
+                        color: amberColor,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.405,
+                        color: amberColor,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     ));
--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 Color wColor = Colors.white;
 Color bColor = Colors.black;
 Color bgColor = Color(0xffe2e2e2);
+Color gColor = Colors.grey;
+Color amberColor = Colors.amber;
