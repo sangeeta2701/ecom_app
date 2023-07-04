9a9088af3dbc2a35d34d622fb787c9326d51f3ce
DELL <anjugupta7060@gmail.com>
2023-07-04 20:44:01 +0530

    Sneaker model

--- lib/models/snekers.dart
+++ lib/models/snekers.dart
@@ -6,7 +6,7 @@ import 'dart:convert';
 
 List<Snekers> snekersFromJson(String str) => List<Snekers>.from(json.decode(str).map((x) => Snekers.fromJson(x)));
 
-String snekersToJson(List<Snekers> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+// String snekersToJson(List<Snekers> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Snekers {
     String id;
@@ -14,7 +14,7 @@ class Snekers {
     String category;
     List<String> imageUrl;
     String oldPrice;
-    List<Size> sizes;
+    List<dynamic> sizes;
     String price;
     String description;
     String title;
@@ -37,41 +37,41 @@ class Snekers {
         category: json["category"],
         imageUrl: List<String>.from(json["imageUrl"].map((x) => x)),
         oldPrice: json["oldPrice"],
-        sizes: List<Size>.from(json["sizes"].map((x) => Size.fromJson(x))),
+        sizes: List<dynamic>.from(json["sizes"].map((x) => x)),
         price: json["price"],
         description: json["description"],
         title: json["title"],
     );
 
-    Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-        "category": category,
-        "imageUrl": List<dynamic>.from(imageUrl.map((x) => x)),
-        "oldPrice": oldPrice,
-        "sizes": List<dynamic>.from(sizes.map((x) => x.toJson())),
-        "price": price,
-        "description": description,
-        "title": title,
-    };
+    // Map<String, dynamic> toJson() => {
+    //     "id": id,
+    //     "name": name,
+    //     "category": category,
+    //     "imageUrl": List<dynamic>.from(imageUrl.map((x) => x)),
+    //     "oldPrice": oldPrice,
+    //     "sizes": List<dynamic>.from(sizes.map((x) => x.toJson())),
+    //     "price": price,
+    //     "description": description,
+    //     "title": title,
+    // };
 }
 
-class Size {
-    String size;
-    bool isSelected;
+// class Size {
+//     String size;
+//     bool isSelected;
 
-    Size({
-        required this.size,
-        required this.isSelected,
-    });
+//     Size({
+//         required this.size,
+//         required this.isSelected,
+//     });
 
-    factory Size.fromJson(Map<String, dynamic> json) => Size(
-        size: json["size"],
-        isSelected: json["isSelected"],
-    );
+//     factory Size.fromJson(Map<String, dynamic> json) => Size(
+//         size: json["size"],
+//         isSelected: json["isSelected"],
+//     );
 
-    Map<String, dynamic> toJson() => {
-        "size": size,
-        "isSelected": isSelected,
-    };
-}
+//     Map<String, dynamic> toJson() => {
+//         "size": size,
+//         "isSelected": isSelected,
+//     };
+// }
